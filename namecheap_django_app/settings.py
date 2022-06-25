e8c8f56471004767f02023e1da52423b028b0f00
mzarka <medzarka@gmail.com>
2022-06-25 23:20:27 +0300

    Just a sample app to test Django apps on namecheap shared hosting 4.

--- namecheap_django_app/settings.py
+++ namecheap_django_app/settings.py
@@ -16,8 +16,8 @@ import sys
 from pathlib import Path
 import environ
 
-from tools import createDir
-from tools import FileLogger
+from namecheap_django_app.tools import createDir
+from namecheap_django_app.tools import FileLogger
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
